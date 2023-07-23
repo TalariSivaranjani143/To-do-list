ff6cf444603a6c4fbecd55e8584f97a3358b9276
TalariSivaranjani143 <136677100+TalariSivaranjani143@users.noreply.github.com>
2023-07-23 12:13:38 +0530

    Add files via upload

---
+++ todolist.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById("input-box")
+const listItems = document.getElementById("listitems")
+
+function task(){
+    if(inputBox.value == ''){
+        alert("Enter the task to do");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listItems.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listItems.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName == "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listItems.innerHTML);
+}
+
+function showTask(){
+    listItems.innerHTML = localStorage.getItem("data");
+}
+showTask();
